15083f638606d97025dce7215c3dd375424179df
Franck Hu <byfranckhu@gmail.com>
2023-04-18 02:45:22 +0200

    style(font): change font

--- src/lib/components/header/Title.svelte
+++ src/lib/components/header/Title.svelte
@@ -1,21 +1,4 @@
-<h1>&lt; <span>Franck</span> Hu /&gt;</h1>
+<h1>Franck Hu</h1>
 
 <style>
-	span {
-		padding: 2px;
-		border-radius: 6px;
-
-		display: inline-block;
-		background-image: radial-gradient(
-			circle 248px at center,
-			#16d9e3 0%,
-			#30c7ec 47%,
-			#46aef7 100%
-		);
-		box-shadow: #26394d 0 20px 30px -10px;
-	}
-
-	:global(body.dark-theme) span {
-		box-shadow: #dae8e6 0 20px 30px -10px;
-	}
 </style>
--- src/lib/components/index/Title.svelte
+++ src/lib/components/index/Title.svelte
@@ -1,18 +1,8 @@
-<h1><span>Franck Hu</span> 👋</h1>
+<h1>Franck Hu 👋</h1>
 
 <style>
 	h1 {
 		margin-block-end: 64px;
 		text-align: center;
-	}
-
-	span {
-		text-shadow: 0 3px 0 #b2a98f, 0 14px 10px rgba(0, 0, 0, 0.15), 0 24px 2px rgba(0, 0, 0, 0.1),
-			0 34px 30px rgba(0, 0, 0, 0.1);
-	}
-
-	:global(body.dark-theme) span {
-		text-shadow: 0 3px 0 #f7e1d5, 0 14px 10px rgba(255, 255, 255, 0.15),
-			0 24px 2px rgba(255, 255, 255, 0.1), 0 34px 30px rgba(255, 255, 255, 0.1);
 	}
 </style>
