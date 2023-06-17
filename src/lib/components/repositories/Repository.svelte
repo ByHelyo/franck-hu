fd3a89e1ab92e6e6237bf9e8c4b0f5c8fc882e4a
Franck Hu <byfranckhu@gmail.com>
2023-06-17 17:52:25 +0200

    refacto(repository): refacto css

--- src/lib/components/repositories/Repository.svelte
+++ src/lib/components/repositories/Repository.svelte
@@ -59,27 +59,42 @@
 		border-radius: 6px;
 		background-color: $light-background-primary;
 		border: 2px solid $light-background-primary-border;
+
+		&:hover {
+			background-color: $light-background-primary-hover;
+		}
+
+		&:focus {
+			background-color: $light-background-primary-focus;
+		}
+
+		/* Fade */
+		&.hovered {
+			vertical-align: middle;
+			-webkit-transform: perspective(1px) translateZ(0);
+			transform: perspective(1px) translateZ(0);
+			box-shadow: 0 0 1px rgba(0, 0, 0, 0);
+			overflow: hidden;
+			-webkit-transition-duration: 0.3s;
+			transition-duration: 0.3s;
+			-webkit-transition-property: color, background-color;
+			transition-property: color, background-color;
+		}
 	}
 
-	a:hover {
-		background-color: $light-background-primary-hover;
-	}
+	:global(body.dark-theme) {
+		a {
+			background-color: $dark-background-primary;
+			border-color: $dark-background-primary-border;
 
-	a:focus {
-		background-color: $light-background-primary-focus;
-	}
+			&:hover {
+				background-color: $dark-background-primary-hover;
+			}
 
-	:global(body.dark-theme) a {
-		background-color: $dark-background-primary;
-		border-color: $dark-background-primary-border;
-	}
-
-	:global(body.dark-theme) a:hover {
-		background-color: $dark-background-primary-hover;
-	}
-
-	:global(body.dark-theme) a:focus {
-		background-color: $dark-background-primary-focus;
+			&:focus {
+				background-color: $dark-background-primary-focus;
+			}
+		}
 	}
 
 	img {
@@ -94,19 +109,5 @@
 	div.details {
 		display: flex;
 		gap: 32px;
-	}
-
-	/* Fade */
-
-	a.hovered {
-		vertical-align: middle;
-		-webkit-transform: perspective(1px) translateZ(0);
-		transform: perspective(1px) translateZ(0);
-		box-shadow: 0 0 1px rgba(0, 0, 0, 0);
-		overflow: hidden;
-		-webkit-transition-duration: 0.3s;
-		transition-duration: 0.3s;
-		-webkit-transition-property: color, background-color;
-		transition-property: color, background-color;
 	}
 </style>
