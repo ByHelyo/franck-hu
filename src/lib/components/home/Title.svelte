a532de16da79c679e0276038b91a14ed8e5cf04f
Franck Hu <byfranckhu@gmail.com>
2023-06-23 22:05:54 +0200

    style(main): change waving hand emoji

--- src/lib/components/home/Title.svelte
+++ src/lib/components/home/Title.svelte
@@ -1,8 +1,17 @@
-<h1>Hi, I'm Franck Hu 👋</h1>
+<p>Hi, I'm Franck Hu <img src="/misc/waving_hand_color_default.svg" alt="" /></p>
 
 <style>
-	h1 {
+	p {
 		margin-block-end: 64px;
 		text-align: center;
+		font-size: 32px;
+		font-weight: bold;
+	}
+
+	img {
+		width: 64px;
+		height: 64px;
+		position: relative;
+		top: 12px;
 	}
 </style>
