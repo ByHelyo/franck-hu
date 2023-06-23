4d47e7b6ea8cdad13370bc0ad5df5c7706ae057d
Franck Hu <byfranckhu@gmail.com>
2023-06-23 21:43:26 +0200

    feat(error): add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,31 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<svelte:head>
+	<title>An error has occured :,(</title>
+</svelte:head>
+
+<main>
+	<div class="status">{$page.status}</div>
+	<div class="status">
+		{$page.error?.message}
+	</div>
+</main>
+
+<style>
+	main {
+		height: 100vh;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		gap: 32px;
+	}
+
+	.status {
+		font-weight: bold;
+		font-size: 64px;
+		text-align: center;
+	}
+</style>
